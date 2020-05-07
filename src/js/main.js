6c9bf20fbe84e804fb21ca482f37bbc1ceb0994d
Arsen Saliev <arsensaliev03@gmail.com>
2020-05-07 21:22:49 +0600

    feat: add search bloks

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,17 @@
-const slider = document.querySelector(".swiper-container");
+window.addEventListener("DOMContentLoaded", () => {
+    const slider = document.querySelector(".swiper-container");
 
-let mySwiper = new Swiper(slider, {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    grabCursor: true,
-    loop: true,
+    new Swiper(slider, {
+        slidesPerView: 4,
+        spaceBetween: 30,
+        grabCursor: true,
+        loop: true,
+
+        // Navigation arrows
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
     
-    // Navigation arrows
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
 });
