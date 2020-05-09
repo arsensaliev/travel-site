a232eb10081695d5d6bada4e166092754b292eac
Arsen Saliev <arsensaliev03@gmail.com>
2020-05-09 12:27:22 +0600

    feat: add feedback block

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,56 @@
 window.addEventListener("DOMContentLoaded", () => {
-    const slider = document.querySelector(".swiper-container");
-
+    const slider = document.querySelector(".swiper-container1");
+    const slider2 = document.querySelector(".swiper-container2");
     new Swiper(slider, {
-        width: 1500,
-        slidesPerView: 3,
+        // slidesPerView: 4,
         spaceBetween: 30,
         grabCursor: true,
+        loop: true,
 
         // Navigation arrows
         navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
+            nextEl: ".popular-button-next",
+            prevEl: ".popular-button-prev",
+        },
+
+        breakpoints: {
+            425: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 2,
+            },
+            1024: {
+                slidesPerView: 3,
+            },
+
+            1440: {
+                slidesPerView: 4,
+            },
         },
     });
-    
+
+    new Swiper(slider2, {
+        // slidesPerView: 4,
+        spaceBetween: 30,
+        grabCursor: true,
+        loop: true,
+
+        // Navigation arrows
+        navigation: {
+            nextEl: ".feedback-button-next",
+            prevEl: ".feedback-button-prev",
+        },
+
+        breakpoints: {
+            425: {
+                slidesPerView: 1,
+            },
+            
+            1024: {
+                slidesPerView: 2,
+            },
+
+        },
+    });
 });
